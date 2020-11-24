a792712a18130bf5491e8125416c1e3661f914cc
Slavik lopush <slavik.lopush@gmail.com>
2020-11-24 21:44:17 +0200

    config webpack, add base header items

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,19 @@
 import React from "react";
-import "./main.scss"
+import "./header.scss";
+import logo from "../../images/logo.png";
+import user from "../../images/user.png";
 
 let MainPage = () => {
 
   return (
     <header>
       <div>Burger here</div>
-      <div>image</div>
-      <div>Login image</div>
+      <div>
+        <img src={logo} />
+      </div>
+      <div>
+        <img src={user} />
+      </div>
     </header>
   );
 };
